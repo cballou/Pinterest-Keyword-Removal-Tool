dcf0c0dcaaa78731a691714a4514883d5703924e
Corey Ballou <ballouc@gmail.com>
2012-10-29 06:57:07 -0400

    Minor updates.

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ This project is currently not tested and is more of a proof of concept.
 
 ## Usage ##
 
-Drag and drop the following bookmarklet to your browser's bookmarks bar.
+Github won't allow the creation of bookmarklet anchors in a README, so you'll need to
+create a new bookmark on your bar and add the following as the url:
 
-<a href="javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.github.com/cballou/Pinterest-Keyword-Removal-Tool/master/pintSizeRemover.js';})();">Pinterest Keyword Removal Bookmarklet</a>
+```bash
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.github.com/cballou/Pinterest-Keyword-Removal-Tool/master/pintSizeRemover.js';})();
+```
--- pintSizeRemover.js
+++ pintSizeRemover.js
@@ -1,25 +1,33 @@
 (function() {
-  var skip = ['weight', 'fat loss', 'workout', 'flat abs', 'nails', 'manicure'];
+    var skip = ['weight', 'fat loss', 'workout', 'flat abs', 'nails', 'manicure'];
 
-  $(document).bind('DOMNodeInserted', function(e) {
-    var $n = $(e.target);
-    if ($n.hasClass('pin')) {
-      removalTest();
+    $(document).bind('DOMNodeInserted', function(e) {
+        var $n = $(e.target);
+        if ($n.hasClass('pin')) {
+            removalTest($n);
+        }
+    });
+
+    /**
+     * Run on load.
+     */
+    $('.pin').each(function() {
+        removalTest($(this));
+    });
+
+    /**
+     * Handles testing the description board names against a set of user-defined
+     * blacklist keywords to see if the pin should be removed.
+     */
+    function removalTest($n) {
+        var desc = $n.find('.description').text();
+        console.log(desc);
+        for (i in skip) {
+            if (desc.indexOf(skip[i]) > -1) {
+	        console.log('Removing:');
+	        console.log(desc);
+	        $n.remove();
+            }
+        } 
     }
-  });
-
-  $('.pin').each(function() {
-    removalTest($(this));
-  });
-
-  function removalTest($n) {
-    var desc = $n.find('.description').text();
-    for (i in skip) {
-      if (desc.indexOf(skip[i]) > -1) {
-	console.log('Removing:');
-	console.log(desc);
-	$n.remove();
-      }
-    } 
-  }
 })();

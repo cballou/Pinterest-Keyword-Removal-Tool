e589df8bc4d1e2e6ce1685b4c59c10ec648ac477
Corey Ballou <ballouc@gmail.com>
2012-10-29 07:37:31 -0400

    Adding in some cool handling for a new button.

--- pintSizeRemover.js
+++ pintSizeRemover.js
@@ -9,6 +9,18 @@
     });
 
     /**
+     * Live binding for removal action.
+     */
+     $('#wrapper').on('click', '.eatPint', function() {
+         var $this = $(this),
+             $pin = $this.closest('.pin'),
+             desc = $pin.find('.description').text().toLowerCase();
+
+         // create the removal tool
+         _log('Clicked pin.');
+     });
+
+    /**
      * Run on load.
      */
     $('.pin:not(.pintSized)').each(removalTest);
@@ -22,6 +34,7 @@
             desc = $this.find('.description').text().toLowerCase();
 
 	$this.addClass('pintSized');
+        $this.find('.PinHolder .actions').append('<a href="Button Button11 WhiteButton ContrastButton eatPint">Hide Keyword(s)</a>');
         for (i in skip) {
             if (desc.indexOf(skip[i]) > -1) {
                 _log('"' + skip[i] + '" keyword found. Removing pin "' + desc + '"');

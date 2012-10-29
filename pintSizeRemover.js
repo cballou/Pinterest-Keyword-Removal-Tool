f64f861cedbc9d456153ff96a5d58315545e3419
Corey Ballou <ballouc@gmail.com>
2012-10-29 07:11:01 -0400

    Updated to handle DOMNodeInsertions.

--- pintSizeRemover.js
+++ pintSizeRemover.js
@@ -3,32 +3,30 @@
 
     $(document).bind('DOMNodeInserted', function(e) {
         var $n = $(e.target);
-        _log($n);
-        if ($n.hasClass('pin') && !$n.hasClass('.pintSized')) {
-            removalTest($n);
+        if ($n.hasClass('BoardLayout')) {
+            $n.find('.pin:not(.pintSized)').each(removalTest);
         }
     });
 
     /**
      * Run on load.
      */
-    $('.pin:not(.pintSized)').each(function() {
-        removalTest($(this));
-    });
+    $('.pin:not(.pintSized)').each(removalTest);
 
     /**
      * Handles testing the description board names against a set of user-defined
      * blacklist keywords to see if the pin should be removed.
      */
-    function removalTest($n) {
-        var desc = $n.find('.description').text().toLowerCase();
+    function removalTest() {
+        var $this = $(this),
+            desc = $this.find('.description').text().toLowerCase();
 	_log('Triggering removal test on: ' + desc);
 
-	$n.addClass('pintSized');
+	$this.addClass('pintSized');
         for (i in skip) {
             if (desc.indexOf(skip[i]) > -1) {
 	        _log('== Removing ==');
-	        $n.remove();
+	        $this.remove();
             }
         } 
     }
